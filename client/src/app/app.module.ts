e419f075b7c1ebe0c9d5241da27198e604e956fa
Dror Cohen <drorcohen@Drors-MBP.lan>
2022-12-19 16:33:42 +0200

    client created

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CategoriesComponent } from './main/components/categories/categories.component';
+import { ArticlesComponent } from './main/components/articles/articles.component';
+import { FavouritesComponent } from './main/components/favourites/favourites.component';
+import { ArticleComponent } from './main/components/articles/article/article.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CategoriesComponent,
+    ArticlesComponent,
+    FavouritesComponent,
+    ArticleComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ client/src/app/core/models/article.model.ts
@@ -0,0 +1,7 @@
+export interface Article {
+  _id?: string;
+  title: string;
+  description: string;
+  image: string;
+  category: string
+}
